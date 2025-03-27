bcb46caaf69dde9a07f3e29f936ea790d665fe03
Aryaman Agrawal <114793920+Aryaman028@users.noreply.github.com>
2025-03-28 04:34:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -7,32 +7,44 @@ using namespace std;
 
 class Solution {
   public:
-    bool check(int node, vector<bool>&visited, vector<vector<int>>& adj){
-        queue<pair<int,int>>q;
+    bool dfs(int node, int parent, vector<bool>&visited, vector<vector<int>>& adj ){
+        visited[node] =true;
         
-        q.push({node,-1});
-        visited[node] = true;
-        
-        while(!q.empty()){
-            
-            auto cur = q.front();
-            int parent = cur.second;
-            int node = cur.first;
-            q.pop();
-            
-            for(auto ele: adj[node]){
-                if(visited[ele]==false){
-                    visited[ele] = true;
-                    q.push({ele,node});
-                }else{
-                    if(parent != ele){
-                        return true;
-                    }
-                }
-            }   
+        for(auto ele : adj[node]){
+            if(!visited[ele]){
+                if(dfs(ele, node, visited, adj))return true;
+            }else if(ele != parent){
+                return true;
+            }
         }
         return false;
-    }
+    }  
+    // bool check(int node, vector<bool>&visited, vector<vector<int>>& adj){
+    //     queue<pair<int,int>>q;
+        
+    //     q.push({node,-1});
+    //     visited[node] = true;
+        
+    //     while(!q.empty()){
+            
+    //         auto cur = q.front();
+    //         int parent = cur.second;
+    //         int node = cur.first;
+    //         q.pop();
+            
+    //         for(auto ele: adj[node]){
+    //             if(visited[ele]==false){
+    //                 visited[ele] = true;
+    //                 q.push({ele,node});
+    //             }else{
+    //                 if(parent != ele){
+    //                     return true;
+    //                 }
+    //             }
+    //         }   
+    //     }
+    //     return false;
+    // }
     bool isCycle(vector<vector<int>>& adj) {
         // Code here
         
@@ -46,11 +58,20 @@ class Solution {
         // neighbour of someoneelse node
         
         // {node,parent}
+        
+        // BFS TRAVERSAL
+        // for(int i = 0; i < n; i++){
+        //     if(visited[i] == false){
+        //         if(check(i, visited,adj))return true;
+        //     }
+        // }
+        
         for(int i = 0; i < n; i++){
             if(visited[i] == false){
-                if(check(i, visited,adj))return true;
+                if(dfs(i,-1, visited,adj))return true;
             }
         }
+        
         return false;
         
     }
